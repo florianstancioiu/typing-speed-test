4800bcb4f34ab55619e73ade83d74d7c395d2f75
Florian Stancioiu <florinvictorstancioiu@gmail.com>
2025-12-28 13:40:29 +0200

    Added a select element to choose page state

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import type { ChangeEvent } from "react";
 
 import Header from "./components/UI/Header/Header";
 import Stages from "./components/Stages/Stages";
@@ -6,11 +7,29 @@ import { TypingContextProvider } from "./store/TypingContext";
 import { type PageState } from "./components/Stages/Stages";
 
 const App = () => {
-  const [pageState, _setPageState] = useState<PageState>("not-started");
+  const [pageState, setPageState] = useState<PageState>("not-started");
+
+  const stages: PageState[] = [
+    "not-started",
+    "started",
+    "high-score-baseline",
+    "high-score-complete",
+    "high-score-smashed",
+  ];
 
   return (
     <>
       <TypingContextProvider>
+        <select
+          onChange={(event: ChangeEvent<HTMLSelectElement>) => {
+            setPageState(event.target.value as PageState);
+          }}
+          className="h-5 mx-auto block mt-4 mb-4"
+        >
+          {stages.map((stage) => (
+            <option key={stage}>{stage}</option>
+          ))}
+        </select>
         <Header />
         <Stages currentStage={pageState} />
       </TypingContextProvider>
