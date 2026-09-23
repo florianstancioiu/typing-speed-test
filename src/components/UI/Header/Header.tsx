502f41d3964eda702fbae5014aed02cf9c16f95f
Florian Stancioiu <florinvictorstancioiu@gmail.com>
2026-09-23 18:29:19 +0300

    Updated the header logo to be a "url"

--- src/components/UI/Header/Header.tsx
+++ src/components/UI/Header/Header.tsx
@@ -1,10 +1,14 @@
 import SmallLogo from "../../../images/logo-small.svg?react";
 import PersonalBest from "../../PersonalBest/PersonalBest";
+import { useTypingContext } from "../../../context/TypingContext";
 
 const Header = () => {
+  const { setStage } = useTypingContext();
+  const logoClickHandler = () => setStage("not-started");
+
   return (
     <header className="p-4 mb-8 flex justify-between items-center md:p-8 md:mb-10 md:pb-0 xl:max-w-7xl xl:mx-auto xl:px-0">
-      <div className="md:flex md:gap-x-2.5 md:items-center">
+      <div onClick={logoClickHandler} className="md:flex md:gap-x-2.5 md:items-center cursor-pointer">
         <SmallLogo />
         <div className="hidden md:block">
           <h1 className="text-white text-2xl font-bold">Typing Speed Test</h1>
