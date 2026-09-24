929d1e2f015a7f2e5d1a4bf447984bcdbabf017f
Florian Stancioiu <florinvictorstancioiu@gmail.com>
2026-09-24 09:11:11 +0300

    Restarted the game when clicking the logo

--- src/components/UI/Header/Header.tsx
+++ src/components/UI/Header/Header.tsx
@@ -1,14 +1,28 @@
 import SmallLogo from "../../../images/logo-small.svg?react";
 import PersonalBest from "../../PersonalBest/PersonalBest";
 import { useTypingContext } from "../../../context/TypingContext";
+import { useStatsContext } from "../../../context/StatsContext";
 
 const Header = () => {
-  const { setStage } = useTypingContext();
-  const logoClickHandler = () => setStage("not-started");
+  const { setStage, setTextThatWasTyped, setIsStarted, setTime } =
+    useTypingContext();
+  const { setAccuracy, setWpm } = useStatsContext();
+
+  const restartTest = () => {
+    setTextThatWasTyped("");
+    setIsStarted(false);
+    setTime(60);
+    setAccuracy(0);
+    setWpm(0);
+    setStage("not-started");
+  };
 
   return (
     <header className="p-4 mb-8 flex justify-between items-center md:p-8 md:mb-10 md:pb-0 xl:max-w-7xl xl:mx-auto xl:px-0">
-      <div onClick={logoClickHandler} className="md:flex md:gap-x-2.5 md:items-center cursor-pointer">
+      <div
+        onClick={restartTest}
+        className="md:flex md:gap-x-2.5 md:items-center cursor-pointer"
+      >
         <SmallLogo />
         <div className="hidden md:block">
           <h1 className="text-white text-2xl font-bold">Typing Speed Test</h1>
